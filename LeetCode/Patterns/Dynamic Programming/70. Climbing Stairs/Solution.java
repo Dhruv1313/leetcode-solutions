035ffbb5a7e1799c076409d7df378a2cc01bcef8
Dhruv1313 <dhruv.120632@stu.upes.ac.in>
2026-08-09 22:46:23 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/70. Climbing Stairs/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/70. Climbing Stairs/Solution.java
@@ -1,14 +1,18 @@
 class Solution {
+    int[] dp;
     public int climbStairs(int n) {
-        if (n==1) return 1;
-        if (n==2) return 2;
-        int first = 1;
-        int second = 2;
-        for(int i=3; i<=n; i++){
-            int current = first + second;
-            first = second;
-            second = current;
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n);
+    }
+    private int solve(int n){
+        if (n<=2){
+            return n;
         }
-    return second;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = solve(n-1) + solve(n-2);
+        return dp[n];
     }
 }
