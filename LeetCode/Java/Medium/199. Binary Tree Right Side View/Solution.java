2a9fa25517d4c54d1677cce6123ddd070deeb0b8
Dhruv1313 <dhruv.120632@stu.upes.ac.in>
2026-08-14 12:03:41 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/199. Binary Tree Right Side View/Solution.java
+++ LeetCode/Java/Medium/199. Binary Tree Right Side View/Solution.java
@@ -10,28 +10,31 @@
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
- *     }
+ *     }i
  * }
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
-        if (root == null)
-        return answer;
+        List<Integer> result = new ArrayList<>();
+        if (root == null){
+            return result;
+        }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        while(!queue.isEmpty()){
+        while( !queue.isEmpty()){
             int size = queue.size();
             for(int i=0; i<size; i++){
-                TreeNode current = queue.poll();
-                if(i==size-1)
-                answer.add(current.val);
-                if (current.left != null)
-                queue.offer(current.left);
-                if(current.right != null)
-                queue.offer(current.right);
+                TreeNode node = queue.poll();
+                if (i== size-1){
+                    result.add(node.val);
+                }
+                if (node.left != null)
+                queue.offer(node.left);
+                if (node.right != null)
+                queue.offer(node.right);
+                
             }
         }
-        return answer;
+         return result;
     }
 }
