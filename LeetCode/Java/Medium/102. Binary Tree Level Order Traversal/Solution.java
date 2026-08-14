9d586ff4e72533db9ed7f0ad87dbb3b235e3eccd
Dhruv1313 <dhruv.120632@stu.upes.ac.in>
2026-08-14 11:56:05 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/102. Binary Tree Level Order Traversal/Solution.java
+++ LeetCode/Java/Medium/102. Binary Tree Level Order Traversal/Solution.java
@@ -15,25 +15,25 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> answer = new ArrayList<>();
-        if(root == null)
-        return answer;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-
         while(!queue.isEmpty()){
             int size = queue.size();
-            List<Integer> list = new LinkedList<>();
-            for(int i= 0; i<size; i++){
-                TreeNode current = queue.poll();
-                list.add(current.val);
-                if(current.left != null)
-                queue.offer(current.left);
-                if( current.right != null)
-                queue.offer(current.right);
+            List<Integer> list = new ArrayList<>();
+            for(int i=0; i<size; i++){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null)
+                queue.offer(node.left);
+                if (node.right != null)
+                queue.offer(node.right);
             }
-            answer.add(list);
+              ans.add(list);
         }
-        return answer;
+        return ans;
     }
 }
