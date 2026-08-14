e10f956428894fc584235794a16c55312d27e01a
Dhruv1313 <dhruv.120632@stu.upes.ac.in>
2026-08-14 19:53:33 +0530

    Add Java Solution to Patterns/Trees

--- LeetCode/Patterns/Trees/784. Insert into a Binary Search Tree/Solution.java
+++ LeetCode/Patterns/Trees/784. Insert into a Binary Search Tree/Solution.java
@@ -15,12 +15,14 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null)
+        if (root == null)
         return new TreeNode(val);
-        if(val<root.val)
-        root.left = insertIntoBST(root.left,val);
-        else root.right = insertIntoBST(root.right,val);
-
-        return root;
+        if (val < root.val){
+            root.left = insertIntoBST(root.left,val);
+        }
+        if (val > root.val){
+            root.right = insertIntoBST(root.right,val);
+        } 
+         return root;
     }
 }
