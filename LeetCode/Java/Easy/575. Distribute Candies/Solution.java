aa7684455fbc6c6f639c01ae2c72eb5fee081994
Dhruv1313 <dhruv.120632@stu.upes.ac.in>
2026-08-20 01:07:44 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/575. Distribute Candies/Solution.java
+++ LeetCode/Java/Easy/575. Distribute Candies/Solution.java
@@ -2,9 +2,10 @@ class Solution {
     public int distributeCandies(int[] candyType) {
         HashSet<Integer> set = new HashSet<>();
         for(int candy: candyType){
-          set.add(candy);
-        
+            set.add(candy);
         }
-        return Math.min(set.size(),candyType.length/2);
+        int distinct = set.size();
+        int half = candyType.length/2;
+        return Math.min(distinct,half);
     }
 }
