aa5ab49fd28b018e46a533b5a36683a008c1e951
Dhruv1313 <dhruv.120632@stu.upes.ac.in>
2026-09-14 01:48:15 +0530

    Add Medium Java Solution for 128. Longest Consecutive Sequence from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/128. Longest Consecutive Sequence/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/128. Longest Consecutive Sequence/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for(int num: nums)
-        set.add(num);
+        for(int num: nums){
+            set.add(num);
+        }   
         int longest = 0;
         for(int num: set){
-            if(!set.contains(num-1)){
+            while(!set.contains(num-1)){
                 int current = num;
                 int length = 1;
-
                 while(set.contains(current+1)){
                     current++;
                     length++;
